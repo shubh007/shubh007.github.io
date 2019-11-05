d04000e9fcf2641aa01ba154b32f8e4e5d041dda
Shubham Singh <shub.singh007@gmail.com>
2019-11-06 02:32:10 +0530

    started

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
-    $scope.helloWorld = "Hello World!!";
+    $scope.name = "Shubham Singh";
+    $scope.cityState = "Bengaluru, Karnataka";
+    $scope.countryPin = "India, PIN - 560068";
+    $scope.mobileEmail = "+91-9980998990 | shub.singh007@gmail.com";
+    $scope.jsfiedleLink = "Github://shubh007";
+    $scope.githubLink = "Jsfiddle://shubham_singh";
+    $scope.linkedIn = "LinkedIn://shub-singh007";
 
 
 
